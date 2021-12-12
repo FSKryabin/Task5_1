1ffbe1520b882286813342d35feaaa703b4c015f
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2021-12-12 04:38:39 +0300

    Добавьте файлы проекта.

---
+++ Task5_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Введите 7 элементоа массива:");
+            Console.WriteLine(Enumerable.Range(0, 7).Select(x => double.Parse(Console.ReadLine())).Average());
+            Console.ReadKey();
+
+        }
+
+    }
+}
